e8597e455cddf1bee213b4659a32f1e6350e1fe1
BleuApple <suchit2309@gmail.com>
2023-09-28 02:00:16 +0530

    Refined Movement
    -Animation of Movement
    -Better movement Input

--- MilanGamev1/Assets/Scripts/Movement.cs
+++ MilanGamev1/Assets/Scripts/Movement.cs
@@ -9,34 +9,25 @@ public class Movement : MonoBehaviour
 
     [SerializeField] private float Speed = 0f;
     [SerializeField] private Rigidbody2D rb;
+    public Animator animator;
 
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-        {
-            velocity = Vector2.up * Speed;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            velocity = Vector2.left * Speed;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            velocity = Vector2.down * Speed;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            velocity = Vector2.right * Speed;
-        }
-        else
-        {
-            velocity = Vector2.zero;
-        }
+        //Input
+        velocity.x = Input.GetAxisRaw("Horizontal");
+        velocity.y = Input.GetAxisRaw("Vertical");
+
+        //Movement floats for animation
+        animator.SetFloat("Horizontal",velocity.x);
+        animator.SetFloat("Vertical",velocity.y);
+        animator.SetFloat("Speed",velocity.sqrMagnitude);
+        
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(velocity.x, velocity.y);
+        //Setting Velocity
+        rb.velocity = new Vector2(velocity.x * Speed * Time.fixedDeltaTime, velocity.y * Speed * Time.fixedDeltaTime);
     }
 }
