837e33beb857d7e7ceaa2f4f711867b7583fb9f1
Ramanuja-Milan23 <ee23btech11223@iith.ac.in>
2023-10-01 02:48:10 +0530

    Small Changes

---
+++ MilanGamev1/Assets/Scripts/Player/InputHandler.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogArea;
+    [SerializeField] private string playerPrompt;
+    [SerializeField] private string expectedInput;
+
+    private bool hasSaidDialog = false;
+    private int currentDialog = 0;
+    private bool isActive = false;
+    private string input = string.Empty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player" && !hasSaidDialog)
+        {
+            currentDialog = 0;
+            isActive = true;
+
+            dialogArea.SetActive(true);
+
+            dialogArea.GetComponentInChildren<TMP_Text>().SetText(playerPrompt);
+        }
+    }
+
+    // Fixed Update is called once per frame
+    void Update()
+    {
+        if(isActive)
+        {
+            foreach (char c in Input.inputString)
+            {
+                if(char.IsNumber(c))
+                {
+                    input += c;
+                }
+            }
+
+            if (input == playerPrompt)
+            {
+                GetComponent<BroadcasterOnTrigger>().trigger();
+            }
+        }
+    }
+}
